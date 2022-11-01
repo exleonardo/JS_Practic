bbd35b84bdc5f2b5d4483c5231a8547555ef64b4
Exleonardo <exleonardo@mail.ru>
2022-11-01 19:04:35 +0300

    Функция среднего значения баллов двух команд

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -4,6 +4,14 @@ const dolphins = calcAverage(44, 23, 71);
 const koalas = calcAverage(65, 54, 49);
 
 console.log(dolphins, koalas);
+// Функция среднего значения баллов двух команд
+function averageTwoComand(dolphins1, koalas1) {
+  const string = console.log(`${(dolphins1 + koalas1) / 2}`);
+  return string;
+}
+const dolphins1 = averageTwoComand(dolphins, koalas);
+const koalas1 = averageTwoComand(65, 54);
+
 //----------------------- LECTURE: Functions -----------------------------------
 function describeCountry(country, population, capitalCity) {
   const string = console.log(
@@ -51,5 +59,33 @@ const resultAfganistan = percentageOfWorld3(afganistan);
 const resultIzrail = percentageOfWorld3(izrail);
 
 console.log(
-  `Население Пакистана ${resultPacistan}, Население Афганистана ${resultAfganistan}, Население Израиля  ${resultIzrail}`
+  `Население Пакистана ${resultPacistan}, 
+  Население Афганистана ${resultAfganistan}, 
+  Население Израиля  ${resultIzrail}`
 );
+//--------- LECTURE: Functions Calling Other Functions ------------
+function percentageOfWorld1(population) {
+  return population / 79;
+}
+function describePopulation(country, population) {
+  const pop = percentageOfWorld1(population);
+  const string = console.log(`В ${country} проживает ${population}млн человек, 
+что составляет около ${pop} населения мира`);
+  return string;
+}
+describePopulation("Китае", 1441);
+describePopulation("Великобритания", 67.33);
+//--------------------------------------------------------------------------------------------------------
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+function repeatStr(n, s) {
+  const string = console.log(s.repeat(n));
+
+  return string;
+}
+repeatStr(6, "I");
+
+//-------------------------------------------------------------------------------------------------------
