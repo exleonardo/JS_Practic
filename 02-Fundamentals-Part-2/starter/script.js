c698e05b65b3596fee06d158caca8a0d2f96fd93
Exleonardo <exleonardo@mail.ru>
2022-11-11 20:41:26 +0300

    LECTURE: Looping Arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,71 @@
 `use strict`;
+let marksWeights = 78;
+let marksHeight = 1.69;
+let johnWeights = 92;
+let johnHeight = 1.95;
+
+let bmiMarks = marksWeights / marksHeight ** 2;
+let bmiJohn = johnWeights / johnHeight ** 2;
+
+let markHigherBMI = bmiMarks > bmiJohn;
+if (markHigherBMI) {
+  console.log(`Индекс БМИ ${bmiMarks} выше, чем ${bmiJohn} `);
+} else {
+  console.log(`Индекс БМИ ${bmiJohn} выше, чем ${bmiMarks}`);
+}
+// -------------------------------------------------------------
+// const dolphins = 19 + 0 + 1;
+// const koals = 19 + 0 + 1;
+
+// const averageDolphins = dolphins / 3;
+// const averageKoals = koals / 3;
+// console.log(averageDolphins, averageKoals);
+// const minScores = 100;
+
+// if (averageDolphins > averageKoals) {
+//   console.log(`team Dolphins score ${averageDolphins}`);
+// } else if (averageDolphins < averageKoals) {
+//   console.log(`team Koals score ${averageKoals}`);
+// } else if (averageDolphins === averageKoals) {
+//   console.log(` Равный результат команд  ${averageKoals} балла`);
+// }
+
+// if (minScores < koals && koals > dolphins) {
+//   console.log(`Team wins koals!!`);
+// } else if (minScores < dolphins && dolphins > koals) {
+//   console.log(`Team wins dolphins!! `);
+// } else if (minScores <= koals && minScores <= dolphins && dolphins === koals) {
+//   console.log(`Победила дружба! очки команд ${dolphins} and ${koals}`);
+// } else {
+//   console.log(`Нет награды командам `);
+// }
+
+//-----------------------------------------------------------------------------
+
+const day = "sanday";
+if (day === "monday") {
+  console.log(`Plan course structure`);
+  console.log(`Go to coding meetup`);
+} else if (day === "tuesday") {
+  console.log(`Prepare theory videos`);
+} else if (day === "wendsday" || day === "thursday") {
+  console.log(`Write code examples`);
+} else if (day === "friday") {
+  console.log(`Record videos`);
+} else if (day === "sanday" || day === "saturday") {
+  console.log("Enjoy the weekend :D");
+} else {
+  console.log("Not a valid day ");
+}
+
+//-------------------------------------------------
+let tip = 0.15;
+const money = 275;
+
+const cash = money >= 50 || money <= 300 ? tip * money : (tip = tip + 0.05) * money;
+console.log(`Окончательная стоимость ${cash + money} , чаевые ${cash}`);
+
+//-----------------------------------------------------------------------
 const calcAverage = (first, second, third) => (first + second + third) / 3;
 const dolphins = calcAverage(23, 34, 27);
 const koalas = calcAverage(65, 54, 49);
@@ -99,19 +166,19 @@ function repeatStr(n, s) {
 repeatStr(6, "I");
 
 //-------------------------------------LECTURE: Introduction to Arrays---------------------------------------------------
-const countries = [143.4, 1441, 257.4, 1393];
-if (countries.length == 4) {
-  console.log(`массив имеет длинну ${countries.length} символов`);
-} else {
-  console.log(`массив не имеет длинну 4 `);
-}
+// const countries = [143.4, 1441, 257.4, 1393];
+// if (countries.length == 4) {
+//   console.log(`массив имеет длинну ${countries.length} символов`);
+// } else {
+//   console.log(`массив не имеет длинну 4 `);
+// }
 
-const percentages = [
-  percentageOfWorld1(countries[0]),
-  percentageOfWorld1(countries[1]),
-  percentageOfWorld1(countries[2]),
-];
-console.log(percentages);
+// const percentages = [
+//   percentageOfWorld1(countries[0]),
+//   percentageOfWorld1(countries[1]),
+//   percentageOfWorld1(countries[2]),
+// ];
+// console.log(percentages);
 //----------------------------- LECTURE: Basic Array Operations (Methods) ----------------------------------------------
 
 const neighbours = [`Russia`, `Germany`, `China`, `Yapon`];
@@ -222,19 +289,82 @@ const lul = {
 console.log(lul.getSummary());
 //-------------
 
-const myCountry = {
-  country: "Russia",
-  capital: "Москва",
-  language: "Russian",
-  population: 143.4,
-  neighbours: [`Belarus`, `Ukraine`, `Polsha`],
-  checkIsland: function () {
-    this.isIsland = this.neighbours.length > 0 ? true : false;
-    return `${this.isIsland}`;
-  },
-  describe: function () {
-    return `В ${this.country} ${this.population} миллионов человек ${this.neighbours.length} соседние страны и столица ${this.capital} `;
+// const myCountry = {
+//   country: "Russia",
+//   capital: "Москва",
+//   language: "Russian",
+//   population: 143.4,
+//   neighbours: [`Belarus`, `Ukraine`, `Polsha`],
+//   checkIsland: function () {
+//     this.isIsland = this.neighbours.length > 0 ? true : false;
+//     return `${this.isIsland}`;
+//   },
+//   describe: function () {
+//     return `В ${this.country} ${this.population} миллионов человек ${this.neighbours.length} соседние страны и столица ${this.capital} `;
+//   },
+// };
+// console.log(myCountry.describe());
+// console.log(myCountry.checkIsland());
+//--------------------------- Coding Challenge #3 ----------------------------
+
+const marksBmi = {
+  name: `Марк`,
+  marksWeight: 78,
+  marksHeight: 1.69,
+  calcBmi: function () {
+    this.bmiMarks = this.marksWeight / this.marksHeight ** 2;
+    return this.bmiMarks;
   },
 };
-console.log(myCountry.describe());
-console.log(myCountry.checkIsland());
+
+const johnBmi = {
+  name: `Джон`,
+  johnWeights: 92,
+  johnHeight: 1.95,
+  calcBmi: function () {
+    this.bmijohn = this.johnWeights / this.johnHeight ** 2;
+    return this.bmijohn;
+  },
+};
+
+if (marksBmi.calcBmi() > johnBmi.calcBmi()) {
+  console.log(
+    `Индекс БМИ ${marksBmi.name} ${marksBmi.calcBmi()} выше, чем ${johnBmi.name} ${johnBmi.calcBmi()} `
+  );
+} else {
+  console.log(
+    `Индекс БМИ ${johnBmi.name} ${johnBmi.calcBmi()} выше, чем ${marksBmi.name} ${marksBmi.calcBmi()}`
+  );
+}
+//------------------ LECTURE: Iteration: The for Loop ---------------------------
+
+for (voter = 1; voter <= 50; voter++) {
+  console.log(`Избиратель номер ${voter} в настоящее время голосует`);
+}
+//--------------------- LECTURE: Looping Arrays, Breaking and Continuing --------------------
+
+function percentageOfWorld1(population) {
+  return population / 79;
+}
+const kitai = percentageOfWorld1(1441);
+const avstralia = percentageOfWorld1(257.4);
+const india = percentageOfWorld1(1393);
+
+console.log(`Процент населения страны китай от мирового ${kitai} `);
+console.log(`Процент населения страны Австралия от мирового ${avstralia} `);
+console.log(`Процент населения страны Индия от мирового ${india} `);
+
+const countries = [143.4, 1441, 257.4, 1393];
+const percentages = [
+  percentageOfWorld1(countries[0]),
+  percentageOfWorld1(countries[1]),
+  percentageOfWorld1(countries[2]),
+  percentageOfWorld1(countries[3]),
+];
+console.log(`массив принимает вид ==> ${percentages}`);
+//--- основная часть задачи на цикл
+const percentages2 = [];
+for (let i = 0; i < countries.length; i++) {
+  percentages2[i] = percentageOfWorld1(countries[i]);
+}
+console.log(percentages2);
